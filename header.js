95ce9d0349473a359851ad47c624e40fb6492ed2
CristianMeSu <55511497+CristianMeSu@users.noreply.github.com>
2022-11-28 22:16:16 -0500

    creacion de HEADER y FOOTER
    se desarrollan modulos de header y footer para la modulacion

---
+++ header.js
@@ -0,0 +1,74 @@
+document.write(`
+<div class="swiper-container swiper-slider swiper-variant-1 bg-black" data-loop="false" data-autoplay="5500" data-simulate-touch="true">
+          <div class="swiper-wrapper text-center">
+            <div class="swiper-slide" data-slide-bg="images/slide-3.jpg">
+              <div class="swiper-slide-caption text-center">
+                <div class="container">
+                  <div class="row justify-content-md-center">
+                    <div class="col-md-11 col-lg-10 col-xl-9">
+                     
+                      <h2 style="font-size: 8.5rem;" class="slider-header css" data-caption-animate="fadeInUp" data-caption-delay="150">BLOG THEME</h2>
+                      <p class="text-bigger slider-text" data-caption-animate="fadeInUp" data-caption-delay="250">will help you solve very complex legal issues.</p>
+                      <div class="button-block __slider-header-menu" data-caption-animate="fadeInUp" data-caption-delay="400">
+                        <ul class="__slider-header-menu">
+                          <li class="__slider-header-item"><a href="">HOME</a></li>
+                          <li class="__slider-header-item"><a href="">BLOG</a></li>
+                          <li class="__slider-header-item"><a href="">CONTACTS</a></li>
+                          <li class="__slider-header-item"><a href="">ABOUT</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide" data-slide-bg="images/slide-2.jpg">
+              <div class="swiper-slide-caption text-center">
+                <div class="container">
+                  <div class="row justify-content-md-center">
+                    <div class="col-md-11 col-lg-10 col-xl-9">
+                     
+                      <h2 style="font-size: 8.5rem;" class="slider-header css" data-caption-animate="fadeInUp" data-caption-delay="150">BLOG THEME</h2>
+                      <p class="text-bigger slider-text" data-caption-animate="fadeInUp" data-caption-delay="250">will help you solve very complex legal issues.</p>
+                      <div class="button-block __slider-header-menu" data-caption-animate="fadeInUp" data-caption-delay="400">
+                        <ul class="__slider-header-menu">
+                          <li class="__slider-header-item"><a href="">HOME</a></li>
+                          <li class="__slider-header-item"><a href="">BLOG</a></li>
+                          <li class="__slider-header-item"><a href="">CONTACTS</a></li>
+                          <li class="__slider-header-item"><a href="">ABOUT</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide" data-slide-bg="images/slide-1.jpg">
+              <div class="swiper-slide-caption text-center">
+                <div class="container">
+                  <div class="row justify-content-md-center">
+                    <div class="col-md-11 col-lg-10 col-xl-9">
+                     
+                      <h2 style="font-size: 8.5rem;" class="slider-header css" data-caption-animate="fadeInUp" data-caption-delay="150">BLOG THEME</h2>
+                      <p class="text-bigger slider-text" data-caption-animate="fadeInUp" data-caption-delay="250">will help you solve very complex legal issues.</p>
+                      <div class="button-block __slider-header-menu" data-caption-animate="fadeInUp" data-caption-delay="400">
+                        <ul class="__slider-header-menu">
+                          <li class="__slider-header-item"><a href="">HOME</a></li>
+                          <li class="__slider-header-item"><a href="">BLOG</a></li>
+                          <li class="__slider-header-item"><a href="">CONTACTS</a></li>
+                          <li class="__slider-header-item"><a href="">ABOUT</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-scrollbar d-lg-none"></div>
+          <div class="swiper-nav-wrap">
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+          </div>
+        </div>
+`)
